370961da93069f28b69d460a3a4d9881e99e6ba1
NishaM2 <nisham2522@gmail.com>
2025-02-21 23:05:14 +0530

    React returns

---
+++ week-6-experiments/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <div>
+      <Header title="nisha1"></Header>
+      <Header title="nisha2"></Header>
+    </div>
+  )
+}
+
+function Header({title}) {
+  return <div>
+    {title}
+  </div>
+}
+
+export default App
