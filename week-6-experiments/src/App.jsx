4831c832bebe92176dd70ab626b617830cb681da
NishaM2 <nisham2522@gmail.com>
2025-02-23 19:58:01 +0530

    minimizing re-rendering

--- week-6-experiments/src/App.jsx
+++ week-6-experiments/src/App.jsx
@@ -1,26 +1,27 @@
 import { useState } from "react"
 
 function App() {
-  const [title, setTitle] = useState("my name is nisha");
-
-  function updateTitle() {
-    setTitle("my name is " + Math.random());
-  }
-
   return (
     <div>
       <div>
-        <button onClick={updateTitle}>update the title</button> <br/>
-        <button onClick={updateTitle}>update the title</button> <br/>
-        <Header title={title}></Header>
+        <HeaderWithButton></HeaderWithButton>
         <Header title="nisha2"></Header>
         <Header title="nisha3"></Header>
-        <Header title="nisha4"></Header>
-        <Header title="nisha5"></Header>
-        <Header title="nisha6"></Header>
       </div>
     </div>
   )
+}
+
+function HeaderWithButton() {
+  const [title, setTitle] = useState("my name is nisha");
+  function updateTitle() {
+    setTitle("my name is " + Math.random());
+  }
+  
+  return <div>
+    <button onClick={updateTitle}>update the title</button> <br/>
+    <Header title={title}></Header>
+  </div>
 }
 
 function Header({title}) {
