826f88a7d35c92495307461b3b2d3b7c8441d5dc
NishaM2 <nisham2522@gmail.com>
2025-02-23 19:55:13 +0530

    updating title

--- week-6-experiments/src/App.jsx
+++ week-6-experiments/src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react"
+
 function App() {
+  const [title, setTitle] = useState("my name is nisha");
+
+  function updateTitle() {
+    setTitle("my name is " + Math.random());
+  }
 
   return (
     <div>
-      <Header title="nisha1"></Header>
-      <Header title="nisha2"></Header>
+      <div>
+        <button onClick={updateTitle}>update the title</button> <br/>
+        <button onClick={updateTitle}>update the title</button> <br/>
+        <Header title={title}></Header>
+        <Header title="nisha2"></Header>
+        <Header title="nisha3"></Header>
+        <Header title="nisha4"></Header>
+        <Header title="nisha5"></Header>
+        <Header title="nisha6"></Header>
+      </div>
     </div>
   )
 }
