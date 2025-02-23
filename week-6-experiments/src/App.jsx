a9ebb6e7c977fbc4bff0323b6b1025d48a79ee21
NishaM2 <nisham2522@gmail.com>
2025-02-23 20:22:02 +0530

    added memo

--- week-6-experiments/src/App.jsx
+++ week-6-experiments/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 function App() {
+  const [title, setTitle] = useState("my name is nisha");
+
+  function updateTitle() {
+    setTitle("my name is " + Math.random());
+  }
+
   return (
     <div>
       <div>
-        <HeaderWithButton></HeaderWithButton>
+        <button onClick={updateTitle}>update the title</button> <br/>
+        <Header title={title}></Header>
         <Header title="nisha2"></Header>
         <Header title="nisha3"></Header>
+        <Header title="nisha4"></Header>
+        <Header title="nisha5"></Header>
+        <Header title="nisha6"></Header>
       </div>
     </div>
   )
 }
 
-function HeaderWithButton() {
-  const [title, setTitle] = useState("my name is nisha");
-  function updateTitle() {
-    setTitle("my name is " + Math.random());
-  }
-  
-  return <div>
-    <button onClick={updateTitle}>update the title</button> <br/>
-    <Header title={title}></Header>
-  </div>
-}
-
-function Header({title}) {
+const Header = React.memo(function Header({title}) {
   return <div>
     {title}
   </div>
-}
+})
 
 export default App
