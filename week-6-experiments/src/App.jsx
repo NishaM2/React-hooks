4ddac43175f9514b98ac57f01f5aa598b9f6503c
NishaM2 <nisham2522@gmail.com>
2025-02-27 01:56:21 +0530

    high level use effects

--- week-6-experiments/src/App.jsx
+++ week-6-experiments/src/App.jsx
@@ -1,30 +1,43 @@
 import { useEffect, useState } from "react"
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    fetch("https://dummyjson.com/todos/random")
-      .then(async function (res) {
-        const json = await res.json();
-        setTodos([{ id: json.id, title: json.todo, description: "No description available" }]);
-      })
-  }, []);
-
-  return(
-    <>
-      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
-    </>
-  )
-}
-
-function Todo({ title, description }){
+  const [count, setCount] = useState(1);
   return <div>
-    <h1>{title}</h1>
-    {description}
+    <button onClick={function() {
+      setCount(1);
+    }}>1</button>
+
+    <button onClick={function() {
+      setCount(2);
+    }}>2</button>
+    
+    <button onClick={function() {
+      setCount(3);
+    }}>3</button>
+
+    <Todo id = {count} />
   </div>
 }
 
+function Todo({id}) {
+  const [todo, setTodo] = useState([]);
 
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?id=" + id)
+      .then(async function (res) {
+        const json = await res.json();
+        setTodo({ id: json[0].id, title: json[0].title, description: "No description available" });
+    })
+  }, [id])
+
+  return <div>
+    <h1>
+      {todo.title}
+    </h1>
+    <h4>
+      {todo.description}
+    </h4>
+  </div>
+}
 
 export default App
