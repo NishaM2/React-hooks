4043399a78301983249d38667f1774295575d0a7
NishaM2 <nisham2522@gmail.com>
2025-02-27 16:53:35 +0530

    useMemo

--- week-6-experiments/src/App.jsx
+++ week-6-experiments/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState();
   const [inputValue, setInputValue] = useState(1);
 
-  let count = 0;
-  for (let i=1; i<=inputValue; i++) {
-    count += i;
-  }
+  let count = useMemo( () => {
+    let finalcount = 0;
+    for (let i=1; i<=inputValue; i++) {
+      finalcount += i;
+    }
+    return finalcount;
+  }, [inputValue]);
 
   return <div>
     <input onChange={function(e) {
